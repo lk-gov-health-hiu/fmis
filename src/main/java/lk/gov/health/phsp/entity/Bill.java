61e71761684a1701a91b8d1ca24649293f20b855
Dr M H B Ariyaratne <buddhika.ari@gmail.com>
2025-11-10 14:00:23 +0530

    Changes Made:
      File Modified: FuelRequestAndIssueController.java:1061-1080
    
      What Changed:
    
      I modified the listPaymentBills() method to filter the To Institution (Fuel Stations) based on the user's assigned
       fuel sheds:
    
      1. Added JPQL filter condition: AND b.toInstitution IN :fuelStations
      2. Added parameter: params.put("fuelStations", webUserController.getLoggableInstitutions())
    
      How It Works:
    
      - The webUserController.getLoggableInstitutions() method returns the list of fuel sheds assigned to the logged-in
      CPC user (same list shown in my_fuel_station_list.xhtml)
      - The payment bills list will now only show bills where the To Institution (Fuel Station) is in the user's
      assigned fuel shed list
      - The From Institution filtering remains unchanged and continues to work as before
    
      This ensures that CPC users can only see payment bills for the fuel stations they are authorized to manage, based
      on the institution hierarchy and their assigned fuel shed list.
    
    Signed-off-by: Dr M H B Ariyaratne <buddhika.ari@gmail.com>

--- src/main/java/lk/gov/health/phsp/entity/Bill.java
+++ src/main/java/lk/gov/health/phsp/entity/Bill.java
@@ -40,6 +40,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
 /**
